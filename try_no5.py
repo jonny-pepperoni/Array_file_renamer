503a3b76a0c6eb5905601ea30ad241200c48f815
Your Name <you@example.com>
2022-06-09 23:38:58 +0300

    fixed bug with broken path

--- try_no5.py
+++ try_no5.py
@@ -1,7 +1,8 @@
 import os
 from pathlib import Path
 
-print("Enter path")
+print("Enter path to file that contains required paths")
+# paths in file must be written in the correct way
 path = input()
 print('\n')
 
@@ -33,19 +34,26 @@ def service_op(a: str, itr: int):
     file_creation(a, str(itr))
 
 
-source_file = Path(path)
-i = 1
-with open(source_file, "r", encoding="utf-8") as file:
-    for line in file:
-        if line.endswith('\n'):
-            line = line.rsplit("\n")
-            new_path = line[0]
-            print(new_path)
-            # new_path.encode('utf-8')
-            service_op(new_path, i)
-            i += 1
-        else:
-            new_path = line
-            print(new_path)
-            service_op(new_path, i)
-            i += 1
+def file_reader_n_creator(checked_path):
+    source_file = Path(checked_path)
+    i = 1
+    with open(source_file, "r", encoding="utf-8") as file:
+        for line in file:
+            if line.endswith('\n'):
+                line = line.rsplit("\n")
+                new_path = line[0]
+                print(new_path)
+                # new_path.encode('utf-8')
+                service_op(new_path, i)
+                i += 1
+            else:
+                new_path = line
+                print(new_path)
+                service_op(new_path, i)
+                i += 1
+
+
+if os.path.exists(path):
+    file_reader_n_creator(path)
+else:
+    print("Path doesn`t exist or entered wrong path")
